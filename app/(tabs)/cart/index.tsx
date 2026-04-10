34b2b914f233c9b14fdf29aec1a8f028d49ae0a6
cami zapata <qestudiar123gmail.com>
2026-04-10 18:12:59 -0500

    FEAT: implemento BotonCompra en CartScreen

--- app/(tabs)/cart/index.tsx
+++ app/(tabs)/cart/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import BotonCompra from '../../../components/BotonCompra'
 
 const CartScreen = () => {
   return (
     <View>
       <Text>CartScreen</Text>
+      <BotonCompra onPress={() => alert('Compra realizada')} />
     </View>
   )
 }
